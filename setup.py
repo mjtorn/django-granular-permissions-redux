27285d7980ea0b9c35d73d4e729a257bfa80284a
Markus Törnqvist <mjt+illyria@skyhood.com>
2011-10-24 11:24:50 +0300

    setup with arbitrary version number

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='django-granular-permissions',
+      version='0.3',
+      description='Django granular (row-level) permissions that inject themselves in a non-invasive fashion (without modifying the Django code)',
+      author='Bartosz Ptaszynski',
+      author_email='yazzgoth@gmail.com',
+      url='http://code.google.com/p/django-granular-permissions/',
+      packages=['django_granular_permissions'],
+ )
+
